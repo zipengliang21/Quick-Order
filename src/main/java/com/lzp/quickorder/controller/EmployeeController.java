f1e294efeeba0051d0801b2c8775b0fda707bbf0
Zipeng Liang <zipengliang21@gmail.com>
2025-09-30 20:52:59 +0800

    Implement disable/enable employee functionality

--- src/main/java/com/lzp/quickorder/controller/EmployeeController.java
+++ src/main/java/com/lzp/quickorder/controller/EmployeeController.java
@@ -106,4 +106,20 @@ public class EmployeeController {
         employeeService.page(pageInfo,queryWrapper);
         return R.success(pageInfo);
     }
+
+    /**
+     * 根据id修改员工信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+        log.info(employee.toString());
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
 }
