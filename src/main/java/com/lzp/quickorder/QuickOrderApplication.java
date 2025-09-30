841863d85b54df9822dd4453cc956f98349a4a41
Zipeng Liang <zipengliang21@gmail.com>
2025-09-30 12:50:32 +0800

    Initialize Project

---
+++ src/main/java/com/lzp/quickorder/QuickOrderApplication.java
@@ -0,0 +1,15 @@
+package com.lzp.quickorder;
+
+import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class QuickOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickOrderApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
