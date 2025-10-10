3455a894295eb9d10d39e52e8e40e720353d68c8
Zipeng Liang <zipengliang21@gmail.com>
2025-10-10 14:53:00 +0800

    Implement Login with SMS Feature

--- src/main/java/com/lzp/quickorder/filter/LoginCheckFilter.java
+++ src/main/java/com/lzp/quickorder/filter/LoginCheckFilter.java
@@ -33,7 +33,9 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "front/**"
+                "/front/**",
+                "/user/sendMsg",
+                "/user/login"
         };
 
         boolean check = check(urls, requestURI);
@@ -51,6 +53,15 @@ public class LoginCheckFilter implements Filter {
             BaseContext.setCurrentId(empId);
 
             filterChain.doFilter(request, response);
+            return;
+        }
+
+        //4-2、判断登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("user"));
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+            filterChain.doFilter(request,response);
             return;
         }
 
