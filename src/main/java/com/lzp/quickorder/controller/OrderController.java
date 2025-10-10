deb15178f68908572469ff5be965e272259f4c0c
Zipeng Liang <zipengliang21@gmail.com>
2025-10-10 22:17:09 +0800

    Implement Submit Order Functionality

--- src/main/java/com/lzp/quickorder/controller/OrderController.java
+++ src/main/java/com/lzp/quickorder/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.lzp.quickorder.controller;
 
+import com.lzp.quickorder.common.R;
+import com.lzp.quickorder.entity.Orders;
 import com.lzp.quickorder.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders){
+        log.info("订单数据：{}",orders);
+        orderService.submit(orders);
+        return R.success("下单成功");
+    }
+
+
 }
--- src/main/java/com/lzp/quickorder/service/OrderService.java
+++ src/main/java/com/lzp/quickorder/service/OrderService.java
@@ -4,4 +4,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.lzp.quickorder.entity.Orders;
 
 public interface OrderService extends IService<Orders> {
+
+    /**
+     * 用户下单
+     * @param orders
+     */
+    public void submit(Orders orders);
+    
 }
